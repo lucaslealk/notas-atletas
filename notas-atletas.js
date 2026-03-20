181fbb43fd442cf970a597993a024960213c5867
Lucas Leal <lucasleal.3011@gmail.com>
2026-03-20 08:46:19 -0300

    first commit

---
+++ notas-atletas.js
@@ -0,0 +1,44 @@
+class Atletas {
+    constructor(atletas) {
+        this.atletas = atletas;
+    }
+    boletimGeral() {
+      // Para cada atleta, ordena as notas, remove a menor e a maior, calcula a média das notas restantes e retorna um array de objetos com o nome do atleta, as notas ordenadas e a média.
+      let resultados = [];
+
+      this.atletas.forEach((atleta) => {
+        let notasComp = [...atleta.notas].sort((a, b) => a - b);
+
+        notasComp = notasComp.slice(1, 4);
+
+        let soma = notasComp.reduce((acc, nota) => acc + nota, 0);
+
+        let media = soma / notasComp.length;
+
+        resultados.push({nome: atleta.nome, notas: atleta.notas.sort((a, b) => a - b), media: media.toFixed(2) });
+      });
+      return resultados;
+      }
+}
+
+let atletas = [
+    {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
+let resultado = new Atletas(atletas);
+console.log(resultado.boletimGeral());
+ 
---
+++ readme.md
@@ -0,0 +1,2 @@
+Projeto criado com o intuito de calcular a média de notas dos participantes de um campeonato. 
+Cada atleta irá receber nota de 5 jurados. 
